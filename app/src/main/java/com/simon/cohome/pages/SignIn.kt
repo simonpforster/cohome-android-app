3139b8a44c89d9917e32de7e316bd4a19b6ae2ca
Simon Forster <sforster@academytrainee.com>
2022-03-27 21:41:03 +0100

    #2 Created functionless sign in page

---
+++ app/src/main/java/com/simon/cohome/pages/SignIn.kt
@@ -0,0 +1,12 @@
+package com.simon.cohome.pages
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.simon.cohome.R
+
+class SignIn: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.sign_in)
+    }
+}
